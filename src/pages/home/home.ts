cf37653a4c1c35599c46054413fc822670345071
Ramón Souza <contato@ramonsouza.com>
2019-01-16 14:38:19 -0200

    lista de carros 'dinamica'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros;
 
+  constructor(public navCtrl: NavController) {
+    this.carros = [
+      {nome: "Opala", preco: 20000},
+      {nome: "Brasília", preco: 12000},
+      {nome: "Monza", preco: 15000},
+      {nome: "Passat", preco: 13500},
+      {nome: "Chevette", preco: 10000},
+    ];
   }
 
 }
