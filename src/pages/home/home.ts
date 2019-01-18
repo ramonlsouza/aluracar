c0b54790f9f34d734b74b022288e2ef9cdcd37f4
Ramón Souza <contato@ramonsouza.com>
2019-01-18 16:22:03 -0200

    loading enquanto dados são carregados

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Carro } from '../../models/carro';
 import { HttpClient } from '@angular/common/http';
 
@@ -11,11 +11,18 @@ export class HomePage {
 
   public carros: Carro[];
 
-  constructor(public navCtrl: NavController, private _http: HttpClient) {
+  constructor(public navCtrl: NavController, private _http: HttpClient, private _loadingCtrl: LoadingController) {
+    let loading = this._loadingCtrl.create({
+      content: 'Aguarde o carregamento dos carros...'
+    });
+
+    loading.present();
+
     this._http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
     .subscribe(
       (carros) => {
         this.carros = carros;
+        loading.dismiss();
       }
     );
   } 
