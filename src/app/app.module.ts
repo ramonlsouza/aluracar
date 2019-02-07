5e26a0ba3ba6c79f2ad68c73d4eb38c273aff36e
Ramón Souza <contato@ramonsouza.com>
2019-02-07 15:34:29 -0200

    lazy loading pagina escolha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,11 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { HttpClientModule } from '@angular/common/http';
 import { CarrosServiceProvider } from '../providers/carros-service/carros-service';
-import { EscolhaPage } from '../pages/escolha/escolha';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    EscolhaPage
   ],
   imports: [
     BrowserModule,
@@ -25,7 +23,6 @@ import { EscolhaPage } from '../pages/escolha/escolha';
   entryComponents: [
     MyApp,
     HomePage,
-    EscolhaPage
   ],
   providers: [
     StatusBar,
