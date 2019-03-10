d9b04907f28fc50daab24ba52972e71d4b7cf227
Ramón Souza <contato@ramonsouza.com>
2019-03-10 16:16:12 -0300

    calculando preço total com base em acessorios selecionados

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -9,15 +9,28 @@ import { Carro } from '../../models/carro';
 })
 export class EscolhaPage {
   public carro: Carro;
-  public acessorios: any = [];
+  public acessorios: Acessorio[];
+  private _precoTotal: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = this.navParams.get('carroSelecionado');
+    this._precoTotal = this.carro.preco;
+
     this.acessorios = [
       {nome: 'Freio ABS', preco: 800},
       {nome: 'Ar-condicionado', preco: 1000},
       {nome: 'MP3 Player', preco: 500},
     ];
+  }
+
+  atualizaTotal(ativado:boolean, acessorio: Acessorio){
+    ativado?
+      this._precoTotal += acessorio.preco:
+      this._precoTotal -= acessorio.preco;
+  }
+  //como atributo é privado, precisa de getter para acessar de fora
+  get precoTotal(){
+    return this._precoTotal;
   }
 
 }
