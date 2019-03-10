986deda6dff8bbde291a1836b98d790ccac4938b
Ramón Souza <contato@ramonsouza.com>
2019-03-10 17:10:54 -0300

    inicio integração agendamento com api

---
+++ src/providers/agendamentos-service/agendamentos-service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class AgendamentosServiceProvider {
+  private _url = 'http://localhost:8080/api';
+
+  constructor(private _http: HttpClient) {
+  }
+
+  agenda(agendamento){
+    return this._http.post(this._url+'/agendamento/agenda',agendamento);
+  }
+
+}
