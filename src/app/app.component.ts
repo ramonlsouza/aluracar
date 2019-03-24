47917c6fce0262e622087766c8c309f3a4e2a13e
Ramón Souza <contato@ramonsouza.com>
2019-03-23 23:37:51 -0300

    dados usuario logado + fix menu na tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { ListaAgendamentosPage } from '../pages/lista-agendamentos/lista-agendamentos';
 import { LoginPage } from '../pages/login/login';
 import { PerfilPage } from '../pages/perfil/perfil';
+import { UsuariosServiceProvider } from '../providers/usuarios-service/usuarios-service';
 @Component({
   selector: 'myapp',
   templateUrl: 'app.html'
@@ -19,7 +20,7 @@ export class MyApp {
     {titulo: 'Perfil', pagina: PerfilPage.name, icone: 'person'}
   ];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private _usuariosService: UsuariosServiceProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -30,6 +31,10 @@ export class MyApp {
 
   irParaPagina(pagina){
     this.nav.push(pagina);
+  }
+
+  get usuarioLogado(){
+    return this._usuariosService.obtemUsuarioLogado();
   }
 }
 
